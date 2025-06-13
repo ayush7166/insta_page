ba2cc37e037dd3a5a6ba6ff7a0efd25b3c543e9f
ayush7166 <ayushbansal7166@gmail.com>
2025-06-13 16:29:12 +0530

    my first server completed

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 const express = require("express");
 const app = express();
+const port =8080;
+app.set("view engine","ejs")
+app.get("/",(req,res)=>{
+    res.render("home");
+});
+app.get("/ig/username",(req,res)=>{
+    let name ="ayush";
+    res.render("username" , {namee : name });
+});
+app.listen(8080 , ()=>{
+    console.log("Server runs on port 8080");
+});
